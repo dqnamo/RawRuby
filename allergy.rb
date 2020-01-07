43cacfe7d8d619136d6eac3a7a7f5898a33cc4e3
John Paul <johnarpaul@gmail.com>
2020-01-07 12:55:25 +0000

    Create allergy.rb

---
+++ allergy.rb
@@ -0,0 +1,9 @@
+puts "Enter Allergy Score:"
+input = gets.to_i
+
+a = { "eggs" => 1, "peanuts" => 2, "shellfish" => 4, "strawberries" => 8, "tomatoes" => 16 }
+
+
+if input%2 != 0
+  puts "eggs"
+end
